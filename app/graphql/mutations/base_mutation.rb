8851c683a31dd3b5636de2d67064f086c8974c6c
Lucas <lucas@prevision.com.br>
2021-05-12 00:11:37 -0300

    add base mut

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -5,5 +5,11 @@ module Mutations
     # field_class Types::BaseField
     # input_object_class Types::BaseInputObject
     # object_class Types::BaseObject
+
+    private
+
+    def set_token_in_context_session(token)
+      context[:session][:token] = token
+    end
   end
 end
