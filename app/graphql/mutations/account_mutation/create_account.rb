201c8e58c09b284c2680dea5cf7aa246c78d35ab
Lucas <lucas@prevision.com.br>
2021-05-12 00:11:58 -0300

    add create acc

--- app/graphql/mutations/account_mutation/create_account.rb
+++ app/graphql/mutations/account_mutation/create_account.rb
@@ -3,8 +3,6 @@ module Mutations
     class CreateAccount < Mutations::BaseMutation
       type Types::AccountType
 
-      # often we will need input types for specific mutation
-      # in those cases we can define those input types in the mutation class itself
       class AuthProviderSignupData < Types::BaseInputObject
         argument :credentials, Inputs::AuthProviderCredentialsInput, required: true
       end
@@ -13,7 +11,7 @@ module Mutations
       argument :auth_provider, AuthProviderSignupData, required: true
 
       def resolve(**service_args)
-        Services::AccountService::CreateAccountService.new.perform(service_args)
+        Services::AccountServices::CreateAccountService.new.perform(service_args)
       end
     end
   end
