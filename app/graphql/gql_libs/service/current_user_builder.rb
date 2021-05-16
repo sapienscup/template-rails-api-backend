c49791c851d7f478a3313bf643117b31c4cbf092
Lucas <lucas@prevision.com.br>
2021-05-16 01:27:20 -0300

    wip

--- app/graphql/gql_libs/service/current_user_builder.rb
+++ app/graphql/gql_libs/service/current_user_builder.rb
@@ -2,7 +2,6 @@ module GqlLibs
   module Service
     class CurrentUserBuilder
       def self.call(request, session)
-        # if we want to change the sign-in strategy, this is the place to do it
         return unless session[:token]
         crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
         token = crypt.decrypt_and_verify(session[:token])
