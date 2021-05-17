c1301081af68389b37c3f12dafd06932f5269d83
Lucas <lucas@prevision.com.br>
2021-05-17 09:38:41 -0300

    Rm

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,9 +76,5 @@ Rails.application.configure do
 
   config.hosts << "walrus_development"
 
-  logger = ActiveSupport::Logger.new(STDOUT)
-  logger.formatter = config.log_formatter
-  config.logger = ActiveSupport::TaggedLogging.new(logger)
-
   config.action_mailer.default_url_options = { host: 'localhost', port: 3025 }
 end
